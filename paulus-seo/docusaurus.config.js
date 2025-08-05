db6616f817db73677a6ae5d52bd05fa289532aa6
paulus-seo <m9472757c@gmail.com>
2025-08-05 15:28:07 +0000

    build: trailingSlash를 false로 변경

--- paulus-seo/docusaurus.config.js
+++ paulus-seo/docusaurus.config.js
@@ -29,8 +29,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'paulus-seo', // Usually your GitHub org/user name.
   projectName: 'paulus-seo.github.io', // Usually your repo name.
-
-  deploymentBranch: 'main',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
